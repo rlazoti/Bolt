a9e89e3116317051b105a969aec418ba72d51db8
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2011-01-25 23:01:09 -0200

    Added support to code coverage with the Cobertura tool

--- src/main/java/br/com/boltframework/error/BoltException.java
+++ src/main/java/br/com/boltframework/error/BoltException.java
@@ -1,20 +1,19 @@
 package br.com.boltframework.error;
 
-
-public class BoltException extends Exception{
+public class BoltException extends Exception {
 
   private static final long serialVersionUID = 1299058916677378267L;
 
-	public BoltException(String message, Throwable cause) {
-		super(message, cause);
-	}
+  public BoltException(String message, Throwable cause) {
+    super(message, cause);
+  }
 
-	public BoltException(Throwable cause) {
-		super(cause);
-	}
+  public BoltException(Throwable cause) {
+    super(cause);
+  }
 
-	public BoltException(String message) {
-		super(message);
-	}
+  public BoltException(String message) {
+    super(message);
+  }
 
 }
