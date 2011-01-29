81aea80568432aeb65082991e47f047a554d0580
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2011-01-29 15:40:28 -0200

    Refactoring of ControllerUtils tests

--- src/test/java/br/com/boltframework/util/ControllerUtilsTest.java
+++ src/test/java/br/com/boltframework/util/ControllerUtilsTest.java
@@ -1,8 +1,13 @@
 package br.com.boltframework.util;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
+
+import br.com.boltframework.core.ControllerMapping;
+import br.com.boltframework.http.HttpMethod;
 import static org.junit.Assert.*;
 
 public class ControllerUtilsTest {
@@ -17,6 +22,23 @@ public class ControllerUtilsTest {
     assertEquals(expected, controller);
   }
 
+  private List<ControllerMapping> getMappingList() {
+    List<ControllerMapping> mappings = new ArrayList<ControllerMapping>();
+    mappings.add(createMapping("home", "index", HttpMethod.GET));
+    mappings.add(createMapping("home", "contact", HttpMethod.POST));
+    mappings.add(createMapping("users", "list", HttpMethod.GET));
+    mappings.add(createMapping("users", "show", HttpMethod.POST));
+    return mappings;
+  }
+
+  private ControllerMapping createMapping(String controllerName, String actionName, HttpMethod httpMethod) {
+    ControllerMapping mapping = new ControllerMapping();
+    mapping.setControllerName(controllerName);
+    mapping.setActionName(actionName);
+    mapping.setHttpMethod(httpMethod);
+    return mapping;
+  }
+
   @Test
   public void testObtainDefaultErrorPageWithMessage() {
     try {
@@ -27,19 +49,19 @@ public class ControllerUtilsTest {
       assertTrue(false);
     }
   }
-  
+
   @Test
   public void testGetControllerMappingByPathWithCorrectPath() {
     testControllerExpected("home/index", "home");
     testControllerExpected("/home/index", "home");
-    testControllerExpected("/home/index/", "home");    
+    testControllerExpected("/home/index/", "home");
   }
-  
+
   @Test
   public void testGetControllerMappingByPathWithTwoLevelsOfCorrectPath() {
     testControllerExpected("orders/items/show", "orders/items");
     testControllerExpected("/orders/items/show", "orders/items");
-    testControllerExpected("orders/items/show/", "orders/items");    
+    testControllerExpected("orders/items/show/", "orders/items");
   }
 
   @Test
@@ -49,7 +71,7 @@ public class ControllerUtilsTest {
     testControllerExpected("/index", null);
     testControllerExpected("index", null);
   }
-  
+
   @Test
   public void testGetControllerMappingByPathWithoutValue() {
     testControllerExpected("/", null);
@@ -63,14 +85,14 @@ public class ControllerUtilsTest {
   public void testGetActionMappingByPathWithCorrectPath() {
     testActionExpected("home/index", "index");
     testActionExpected("/home/index", "index");
-    testActionExpected("/home/index/", "index");    
+    testActionExpected("/home/index/", "index");
   }
 
   @Test
   public void testGetActionMappingByPathWithTwoLevelsOfCorrectPath() {
     testActionExpected("orders/items/show", "show");
     testActionExpected("/orders/items/show", "show");
-    testActionExpected("orders/items/show/", "show");    
+    testActionExpected("orders/items/show/", "show");
   }
 
   @Test
@@ -88,6 +110,48 @@ public class ControllerUtilsTest {
     testActionExpected(" ", null);
     testActionExpected("    ", null);
     testActionExpected(null, null);
+  }
+
+  @Test
+  public void testFindMappingWithCorrectValue() {
+    ControllerMapping mapping = ControllerUtils.findMapping(getMappingList(), "/home/index", HttpMethod.GET);
+    assertNotNull(mapping);
+  }
+  
+  @Test
+  public void testFindMappingWithCorrectValue2() {
+    ControllerMapping mapping = ControllerUtils.findMapping(getMappingList(), "/users/show", HttpMethod.POST);
+    assertNotNull(mapping);
+  }
+
+  @Test
+  public void testFindMappingWithoutCorrectValue() {
+    ControllerMapping mapping = ControllerUtils.findMapping(getMappingList(), "/users/login", HttpMethod.GET);
+    assertNull(mapping);
+  }
+
+  @Test
+  public void testFindMappingWithoutValue() {
+    ControllerMapping mapping = ControllerUtils.findMapping(getMappingList(), "/", HttpMethod.GET);
+    assertNull(mapping);
+  }
+
+  @Test
+  public void testFindMappingWithoutValue2() {
+    ControllerMapping mapping = ControllerUtils.findMapping(getMappingList(), "", HttpMethod.GET);
+    assertNull(mapping);
+  }
+
+  @Test
+  public void testFindMappingWithoutValue3() {
+    ControllerMapping mapping = ControllerUtils.findMapping(getMappingList(), null, HttpMethod.GET);
+    assertNull(mapping);
+  }
+
+  @Test
+  public void testFindMappingWithoutValue4() {
+    ControllerMapping mapping = ControllerUtils.findMapping(getMappingList(), "/home", HttpMethod.GET);
+    assertNull(mapping);
   }
 
 }
